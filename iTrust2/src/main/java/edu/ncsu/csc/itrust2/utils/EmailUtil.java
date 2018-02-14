e1e2310b890ba93cc38b234ad8167afeb2b78730
Apeksha <Apeksha@10.137.55.2>
2018-02-14 15:29:20 -0500

    Attempting to fix the failing test in APIPasswordTest by fixing something in EmailUtil (re: #13)

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/utils/EmailUtil.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/utils/EmailUtil.java
@@ -1,5 +1,7 @@
 package edu.ncsu.csc.itrust2.utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -21,78 +23,80 @@ import javax.mail.internet.MimeMessage;
  */
 public class EmailUtil {
 
-    /**
-     * Send an email from the email account in the system's `email.properties`
-     * file
-     * 
-     * @param addr
-     *            Address to send to
-     * @param subject
-     *            Subject of the email
-     * @param body
-     *            Body of the message to send
-     * @throws MessagingException
-     *             Results from JavaX Mail functionality
-     */
-    public static void sendEmail ( final String addr, final String subject, final String body )
-            throws MessagingException {
+	/**
+	 * Send an email from the email account in the system's `email.properties`
+	 * file
+	 * 
+	 * @param addr
+	 *            Address to send to
+	 * @param subject
+	 *            Subject of the email
+	 * @param body
+	 *            Body of the message to send
+	 * @throws MessagingException
+	 *             Results from JavaX Mail functionality
+	 */
+	public static void sendEmail ( final String addr, final String subject, final String body )
+			throws MessagingException {
 
-        InputStream input = null;
+		InputStream input = null;
 
-        final String to = addr;
-        final String from;
-        final String username;
-        final String password;
-        final String host;
+		final String to = addr;
+		final String from;
+		final String username;
+		final String password;
+		final String host;
 
-        final Properties properties = new Properties();
+		final Properties properties = new Properties();
 
-        final String filename = "email.properties";
-        input = DBUtil.class.getClassLoader().getResourceAsStream( filename );
-        if ( null != input ) {
-            try {
-                properties.load( input );
-            }
-            catch ( final IOException e ) {
-                e.printStackTrace();
-            }
-        }
-        from = properties.getProperty( "from" );
-        username = properties.getProperty( "username" );
-        password = properties.getProperty( "password" );
-        host = properties.getProperty( "host" );
+		final String filename = "src/main/java/email.properties";
+		try {
+			input = new FileInputStream(filename); //DBUtil.class.getClassLoader().getResourceAsStream( filename );
 
-        /*
-         * Source for java mail code:
-         * https://www.tutorialspoint.com/javamail_api/
-         * javamail_api_gmail_smtp_server.htm
-         */
+			properties.load( input );
 
-        final Properties props = new Properties();
-        props.put( "mail.smtp.auth", "true" );
-        props.put( "mail.smtp.starttls.enable", "true" );
-        props.put( "mail.smtp.host", host );
-        props.put( "mail.smtp.port", "587" );
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		from = properties.getProperty( "from" );
+		username = properties.getProperty( "username" );
+		password = properties.getProperty( "password" );
+		host = properties.getProperty( "host" );
 
-        final Session session = Session.getInstance( props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication () {
-                return new PasswordAuthentication( username, password );
-            }
-        } );
+		/*
+		 * Source for java mail code:
+		 * https://www.tutorialspoint.com/javamail_api/
+		 * javamail_api_gmail_smtp_server.htm
+		 */
 
-        try {
-            final Message message = new MimeMessage( session );
-            message.setFrom( new InternetAddress( from ) );
-            message.setRecipients( Message.RecipientType.TO, InternetAddress.parse( to ) );
-            message.setSubject( subject );
-            message.setText( body );
-            Transport.send( message );
-        }
-        catch ( final MessagingException e ) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
+		final Properties props = new Properties();
+		props.put( "mail.smtp.auth", "true" );
+		props.put( "mail.smtp.starttls.enable", "true" );
+		props.put( "mail.smtp.host", host );
+		props.put( "mail.smtp.port", "587" );
+
+		final Session session = Session.getInstance( props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication () {
+				return new PasswordAuthentication( username, password );
+			}
+		} );
+
+		try {
+			final Message message = new MimeMessage( session );
+			message.setFrom( new InternetAddress( from ) );
+			message.setRecipients( Message.RecipientType.TO, InternetAddress.parse( to ) );
+			message.setSubject( subject );
+			message.setText( body );
+			Transport.send( message );
+		}
+		catch ( final MessagingException e ) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
 
 }
--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/apitest/APIPasswordTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/apitest/APIPasswordTest.java
@@ -5,6 +5,16 @@ import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Scanner;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +36,7 @@ import edu.ncsu.csc.itrust2.forms.personnel.PasswordChangeForm;
 import edu.ncsu.csc.itrust2.forms.personnel.PersonnelForm;
 import edu.ncsu.csc.itrust2.models.enums.Role;
 import edu.ncsu.csc.itrust2.models.enums.State;
+import edu.ncsu.csc.itrust2.models.persistent.PasswordResetToken;
 import edu.ncsu.csc.itrust2.models.persistent.Personnel;
 import edu.ncsu.csc.itrust2.models.persistent.User;
 import edu.ncsu.csc.itrust2.mvc.config.WebMvcConfiguration;
@@ -124,6 +135,84 @@ public class APIPasswordTest {
                 .content( "patientPW" ) ).andExpect( status().isBadRequest() );
 
     }
+    /*
+     * Credit for checking email:
+     * https://www.tutorialspoint.com/javamail_api/javamail_api_checking_emails.
+     * htm
+     */
+//    private PasswordResetToken getTokenFromEmail () {
+//        final String username = "csc326s182034@gmail.com";
+//        final String password = "csc326s182034@gmail.com";
+//        final String host = "smtp.gmail.com";
+//        PasswordResetToken token = null;
+//        try {
+//            // create properties field
+//            final Properties properties = new Properties();
+//            properties.put( "mail.store.protocol", "pop3" );
+//            properties.put( "mail.pop3.host", host );
+//            properties.put( "mail.pop3.port", "995" );
+//            properties.put( "mail.pop3.starttls.enable", "true" );
+//            final Session emailSession = Session.getDefaultInstance( properties );
+//            // emailSession.setDebug(true);
+//
+//            // create the POP3 store object and connect with the pop server
+//            final Store store = emailSession.getStore( "pop3s" );
+//
+//            store.connect( host, username, password );
+//
+//            // create the folder object and open it
+//            final Folder emailFolder = store.getFolder( "INBOX" );
+//            emailFolder.open( Folder.READ_WRITE );
+//
+//            // retrieve the messages from the folder in an array and print it
+//            final Message[] messages = emailFolder.getMessages();
+//            Arrays.sort( messages, ( x, y ) -> {
+//                try {
+//                    return y.getSentDate().compareTo( x.getSentDate() );
+//                }
+//                catch ( final MessagingException e ) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//                return 0;
+//            } );
+//            for ( final Message message : messages ) {
+//                // SUBJECT
+//                if ( message.getSubject() != null && message.getSubject().contains( "iTrust2 Password Reset" ) ) {
+//                    String content = (String) message.getContent();
+//                    content = content.replaceAll( "\r", "" ); // Windows
+//                    content = content.substring( content.indexOf( "?tkid=" ) );
+//
+//                    final Scanner scan = new Scanner( content.substring( 6, content.indexOf( '\n' ) ) );
+//                    System.err.println( "token(" + content.substring( 6, content.indexOf( '\n' ) ) + ")end" );
+//                    final long tokenId = scan.nextLong();
+//                    scan.close();
+//
+//                    content = content.substring( content.indexOf( "temporary password: " ) );
+//                    content = content.substring( 20, content.indexOf( "\n" ) );
+//                    content.trim();
+//
+//                    if ( content.endsWith( "\n" ) ) {
+//                        content = content.substring( content.length() - 1 );
+//                    }
+//
+//                    token = new PasswordResetToken();
+//                    token.setId( tokenId );
+//                    token.setTempPasswordPlaintext( content );
+//                    break;
+//                }
+//            }
+//
+//            // close the store and folder objects
+//            emailFolder.close( false );
+//            store.close();
+//            return token;
+//        }
+//        catch ( final Exception e ) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
     
     @WithMockUser ( username = "patientPW", roles = { "USER", "ADMIN" } )
     @Test
@@ -153,6 +242,15 @@ public class APIPasswordTest {
         assertTrue( pe.matches( "123456", user.getPassword() ) );
         mvc.perform( post( "/api/v1/requestPasswordReset" ).contentType( MediaType.APPLICATION_JSON )
                 .content( user.getUsername() ) ).andExpect( status().isOk() );
+//        final PasswordChangeForm pchange = new PasswordChangeForm();
+//        PasswordResetToken tokenId = new PasswordResetToken();
+//        tokenId = getTokenFromEmail();
+       
+//        mvc.perform( post( "/api/v1/resetPassword/?tkid=" + tokenId.getId() ).contentType( MediaType.APPLICATION_JSON )
+//                .content( TestUtils.asJsonString( pchange ) ) ).andExpect( status().isOk() );
+        final Personnel p = Personnel.getByName( user );
+        p.delete();
+        user.delete();
     }
 
 }
