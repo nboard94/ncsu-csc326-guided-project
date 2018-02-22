d886c89087ad4516059e4d875a0dabee4df6b4be
Apeksha <Apeksha@10.137.55.2>
2018-02-21 23:56:09 -0500

    Added functionality for UC14 to send an email when a user changes their password. re:#26

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/APIPasswordController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/api/APIPasswordController.java
@@ -64,7 +64,38 @@ public class APIPasswordController extends APIController {
                 user.save();
                 LoggerUtil.log( TransactionType.PASSWORD_UPDATE_SUCCESS, user.getUsername(),
                         "Successfully changed password for user " + user.getUsername() );
-                return new ResponseEntity( successResponse( "Password changed successfully" ), HttpStatus.OK );
+                
+                String addr = "";
+                String firstName = "";
+                final Personnel person = Personnel.getByName( user );
+                if ( person != null ) {
+                    addr = person.getEmail();
+                    firstName = person.getFirstName();
+                }
+                else {
+                    final Patient patient = Patient.getPatient( user );
+                    if ( patient != null ) {
+                        addr = patient.getEmail();
+                        firstName = patient.getFirstName();
+                    }
+                    else {
+                        throw new Exception( "No Patient or Personnel on file for " + user.getId() );
+                    }
+                }
+
+                String body = "Hello " + firstName + ", \n\nYou have successfully updated your password for iTrust2.\n";
+                body += "\nIf you did not request a password reset, please contact a system administrator.\n\n--iTrust2 Admin";
+
+                try {
+                    EmailUtil.sendEmail( addr, "iTrust2 Password Update Success", body );
+                    LoggerUtil.log( TransactionType.PASSWORD_UPDATE_SUCCESS, user.getUsername(),
+                            "Successfully updated password for user " + user.getUsername() );
+                }
+                catch ( final MessagingException e ) {
+                    LoggerUtil.log( TransactionType.EMAIL_NOT_SENT, user.getUsername(),
+                            "Password update email could not be sent for user " + user.getUsername() );
+}
+                return new ResponseEntity( successResponse( "Password updated successfully" ), HttpStatus.OK );
             }
 
             LoggerUtil.log( TransactionType.PASSWORD_UPDATE_FAILURE, user.getUsername(),
